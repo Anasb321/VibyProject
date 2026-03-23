2de1629ef8c97e6ab9ad6c06541814840c41015b
Anasb321 <6330360@cegepmv.ca>
2026-03-22 22:11:33 -0400

    Premiere remise

---
+++ VibyAppProject/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace VibyAppProject
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
