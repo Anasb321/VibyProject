856e6fd1b1f8829e4f5471fbb00be336d1afa3ec
KersenJ-Project <nylekersenjoseph@gmail.com>
2026-03-22 22:15:59 -0400

    Page inscription et Hash mdp

---
+++ WpfApp/WpfApp/HashageService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCrypt.Net;
+
+namespace WpfApp
+{
+    class HashageService
+    {
+        public static string HacherMDP(string motDePasse)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(motDePasse);
+        }
+
+        public static bool VerifierMDP(string motDePasseSaisi, string motDePasseStocke)
+        {
+            return BCrypt.Net.BCrypt.Verify(motDePasseSaisi, motDePasseStocke);
+        }
+    }
+}
