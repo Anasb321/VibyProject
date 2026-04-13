2413c08b62bb0eee3314391799a8fd6283ed7b43
SamuelDom <samueldomesca@outlook.com>
2026-04-12 21:18:39 -0400

    Ajout AfficherRechercheAPI

---
+++ VibyApp.API/AfficherRechercheAPI.cs
@@ -0,0 +1,32 @@
+﻿namespace VibyApp.API
+{
+    using System.Net.Http;
+    using System.Text.Json;
+
+    public class DeezerRechercher
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<List<string>> GetGenresAsync()
+        {
+            try
+            {
+                string json = await _httpClient.GetStringAsync("https://api.deezer.com/genre");
+
+                using JsonDocument doc = JsonDocument.Parse(json);
+                var genres = new List<string>();
+
+                foreach (var element in doc.RootElement.GetProperty("data").EnumerateArray())
+                {
+                    genres.Add(element.GetProperty("name").GetString());
+                }
+
+                return genres;
+            }
+            catch (Exception e)
+            {
+                return new List<string> { "Erreur de chargement" };
+            }
+        }
+    }
+}
