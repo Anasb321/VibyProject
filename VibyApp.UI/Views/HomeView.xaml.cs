40d3b3107d3a47e76e53fe38f0a22a1908012edd
Cupidon <1880160@collegemv.qc.ca>
2026-04-15 11:29:11 -0400

    Fonctionnalité -Afficher top 10 artiste & -Afficher top 10 musique completé

--- VibyApp.UI/Views/HomeView.xaml.cs
+++ VibyApp.UI/Views/HomeView.xaml.cs
@@ -1,25 +1,140 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Windows;
 using System.Windows.Controls;
-using VibyApp.ViewModels;
 
-namespace VibyApp.UI.Views
+namespace VibyApp.UI.Views;
+
+public class DeezerService
 {
-    /// <summary>
-    /// Interaction logic for HomeView.xaml
-    /// </summary>
-    public partial class HomeView : UserControl
-    {
-        public HomeView()
-        {
-            InitializeComponent();
-        }
+    private readonly HttpClient _httpClient;
 
-        private void Profile_Click(object sender, RoutedEventArgs e)
+    public DeezerService()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public async Task<(List<Track> TopTracks, List<Artist> TopArtists)> GetTop10Async()
+    {
+        try
         {
-            if (Application.Current.MainWindow.DataContext is MainViewModel mainVM)
-            {
-                mainVM.NavigateToProfile();
-            }
+            var response = await _httpClient.GetStringAsync("https://api.deezer.com/chart/0/");
+            var chartData = JsonSerializer.Deserialize<DeezerChartResponse>(response);
+
+            var topTracks = chartData?.Tracks?.Data?.Take(10).ToList() ?? new List<Track>();
+            var topArtists = chartData?.Artists?.Data?.Take(10).ToList() ?? new List<Artist>();
+
+            return (topTracks, topArtists);
+        }
+        catch
+        {
+            return (new List<Track>(), new List<Artist>());
         }
     }
+}
+
+public partial class HomeView : UserControl, INotifyPropertyChanged
+{
+    private readonly DeezerService _deezerService;
+
+    private bool _isLoading;
+
+    public HomeView()
+    {
+        InitializeComponent();
+
+        _deezerService = new DeezerService();
+        TopTracks = new ObservableCollection<Track>();
+        TopArtists = new ObservableCollection<Artist>();
+
+        DataContext = this;
+        Loaded += HomeView_Loaded;
+    }
+
+    public ObservableCollection<Track> TopTracks { get; set; }
+    public ObservableCollection<Artist> TopArtists { get; set; }
+
+    public bool IsLoading
+    {
+        get => _isLoading;
+        set
+        {
+            _isLoading = value;
+            OnPropertyChanged();
+        }
+    }
+
+    // Ces méthodes doivent être dans la classe, pas dans une autre méthode !
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private async void HomeView_Loaded(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            IsLoading = true;
+            var (tracks, artists) = await _deezerService.GetTop10Async();
+
+            // On vide et on remplit les collections
+            TopTracks.Clear();
+            foreach (var track in tracks) TopTracks.Add(track);
+
+            TopArtists.Clear();
+            foreach (var artist in artists) TopArtists.Add(artist);
+        }
+        catch (Exception ex)
+        {
+            // Log l'erreur si besoin : Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            IsLoading = false;
+        }
+    } // Fin de HomeView_Loaded
+
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+} // Fin de la classe HomeView
+
+// --- MODELS ---
+public class DeezerChartResponse
+{
+    [JsonPropertyName("tracks")] public ChartSection<Track> Tracks { get; set; }
+
+    [JsonPropertyName("artists")] public ChartSection<Artist> Artists { get; set; }
+}
+
+public class ChartSection<T>
+{
+    [JsonPropertyName("data")] public List<T> Data { get; set; }
+}
+
+public class Track
+{
+    [JsonPropertyName("title")] public string Title { get; set; }
+
+    [JsonPropertyName("artist")] public Artist Artist { get; set; }
+
+    [JsonPropertyName("album")] public Album Album { get; set; }
+
+    [JsonPropertyName("link")] public string LinkTrack { get; set; }
+}
+
+public class Artist
+{
+    [JsonPropertyName("name")] public string Name { get; set; }
+
+    [JsonPropertyName("picture_medium")] public string PictureUrl { get; set; }
+
+    [JsonPropertyName("link")] public string LinkArtist { get; set; }
+}
+
+public class Album
+{
+    [JsonPropertyName("cover_medium")] public string CoverUrl { get; set; }
 }
