6bb13ea9dbf71e992b1dec59349fc45ebdc0a9c7
KersenJ-Project <nylekersenjoseph@gmail.com>
2026-05-03 22:54:54 -0400

    Ajout de la methode pour Retirer Artiste Favoris, je la lierai au frontend quand la page artiste et la page chanson sera faite

---
+++ VibyApp.DB/Migrations/20260504024434_UpdateApresProblemeMain.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VibyApp.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateApresProblemeMain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "MotDePasse",
+                value: "$2a$11$il3QAwJKmK8MRBA.UKzq4eX/keDhq4RqZFruFno/a/uOpEF9EUFfK");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "MotDePasse",
+                value: "$2a$11$azVxUWCLSpeP2pdjmbJe2ub3FXQiF9M8/lnjga85Vcy.UUsXaippa");
+        }
+    }
+}
