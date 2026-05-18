301ed7e42b9ea129afffbfa3352ea1476606bcde
SamuelDom <samueldomesca@outlook.com>
2026-05-18 16:37:37 -0400

    Partie 2 - Barre de play

--- VibyApp.UI/ViewModels/BaseViewModel.cs
+++ VibyApp.UI/ViewModels/BaseViewModel.cs
@@ -1,16 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 
 namespace VibyApp.UI.ViewModels
 {
-    public class BaseViewModel : INotifyPropertyChanged
+    public partial class BaseViewModel : ObservableObject
     {
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
     }
 }
